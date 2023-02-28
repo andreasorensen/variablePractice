45ea2d0e604c03f428ea726a34079eb44957fa21
Andrea Sorensen <dreasor2987@icloud.com>
2023-02-28 15:12:56 -0800

    Change string values & Add console.log statements)

--- variables.js
+++ variables.js
@@ -2,6 +2,10 @@ var location = "Willow Creek"
 var favoriteFood = "Poke Bowl"
 var importantPeople = "Parents"
 
+location = "San Diego"
+favoriteFood = "Sushi"
+importantPeople = "Partner"
+
 var numberOfpets = 4
 var yearsCoding = 0
 var phoneNumber = 7072965418
@@ -9,3 +13,8 @@ var phoneNumber = 7072965418
 var movingSoon = false
 var gardening = true
 var happyRelationship = true    
+
+console.log(location)
+console.log(favoriteFood)
+console.log(importantPeople)
+
