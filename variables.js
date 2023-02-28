b2e94dab09fbeb0ffd73ad6fad8047d7aeedc9c0
Andrea Sorensen <dreasor2987@icloud.com>
2023-02-28 15:08:39 -0800

    Add 3 boolean variables

--- variables.js
+++ variables.js
@@ -5,3 +5,7 @@ var importantPeople = "Parents"
 var numberOfpets = 4
 var yearsCoding = 0
 var phoneNumber = 7072965418
+
+var movingSoon = false
+var gardening = true
+var happyRelationship = true    
